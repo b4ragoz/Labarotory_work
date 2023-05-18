b7836fd7ae98fe85fb1aaca235958cc386a06f71
b4ragoz <133875298+b4ragoz@users.noreply.github.com>
2023-05-18 21:37:51 +0300

    Create linq2.cs

---
+++ linq2.cs
@@ -0,0 +1,33 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] mass = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+        var plus_m = from i in mass where i > 0 select i;
+        Console.WriteLine("Положительные");
+        foreach (var i in plus_m) { Console.WriteLine(i); }
+        var minus_summ = (from i in mass where i < 0 select i).Sum();
+        Console.WriteLine("Сумма отрицательных {0}", minus_summ);
+        var count = (from i in mass where i % 5 == 0 select i).Count();
+        Console.WriteLine("Количество чисел кратных 5 {0}", count);
+        Console.WriteLine("Массив после обработки запросов ");
+        foreach (var i in mass) Console.WriteLine(i); 
+        Console.WriteLine("Удаление всех четных элементов");
+        for(int  i = 0; i < mass.Length; i++) 
+        {
+            if (mass[i] % 2 == 0)
+            {
+                mass[i] = default;
+            }
+        }
+        foreach (var i in mass) Console.WriteLine(i);       
+        var plus_m1 = from i in mass where i > 0 select i;
+        Console.WriteLine("Положительные");
+        foreach (var i in plus_m) { Console.WriteLine(i); }
+        var minus_summ1 = (from i in mass where i < 0 select i).Sum();
+        Console.WriteLine("Сумма отрицательных {0}", minus_summ1);
+        var count1 = (from i in mass where i % 5 == 0 select i).Count();
+        Console.WriteLine("Количество чисел кратных 5 {0}", count1);
+
+    }
+}
